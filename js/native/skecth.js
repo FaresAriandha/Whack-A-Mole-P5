da52cbac0046f1f64507f50ec56ca10cdae37101
FaresAriandha <87983539+FaresAriandha@users.noreply.github.com>
2023-01-16 11:59:05 +0700

    yes

--- js/native/skecth.js
+++
@@ -1,8 +0,0 @@
-// Template untuk p5.js
-function setup() {
-	// ini tempat untuk inisialisasi yang hanya dijalankan sekali saja
-}
-
-function draw() {
-	// ini tempat kita running programnya yak
-}
